0474ab6c39eea7881e64bb01bf41e463b8b30e1e
AlmondBreez3 <bee1162@naver.com>
2025-01-23 11:56:46 +0900

    feat: layout 색상 변경

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,23 @@
-import "./globals.css"
-import { Inter } from "next/font/google"
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Mobile View Layout",
-    description: "A web application with mobile-first layout",
-}
+  title: "Mobile View Layout",
+  description: "A web application with mobile-first layout",
+};
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="ko">
-        <body className={inter.className}>
-        <div className="flex items-center justify-center min-h-screen bg-black">
-            <div className="w-full h-full max-w-[375px] max-h-[812px] bg-white overflow-y-auto shadow-lg">{children}</div>
+  return (
+    <html lang="ko">
+      <body className={inter.className}>
+        <div className="flex items-center justify-center min-h-screen bg-beige">
+          <div className="w-full h-full max-w-[375px] max-h-[812px] bg-white overflow-y-auto shadow-lg">
+            {children}
+          </div>
         </div>
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
-
