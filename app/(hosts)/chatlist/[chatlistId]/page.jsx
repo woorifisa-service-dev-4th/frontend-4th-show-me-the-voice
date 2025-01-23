0502e8402494db06d8042bbc21a91f032e7098f8
sengjun0624 <sengjun0624@naver.com>
2025-01-23 17:23:40 +0900

    delete: chakra 관련 파일 삭제

--- app/(hosts)/chatlist/[chatlistId]/page.jsx
+++ app/(hosts)/chatlist/[chatlistId]/page.jsx
@@ -3,6 +3,7 @@
 import { Suspense } from "react";
 import Card from "@/app/components/Card";
 
+
 // Skeleton Loader Component
 const SkeletonCard = () => {
     return (
@@ -32,10 +33,9 @@ export default function Layout() {
 
                 {/* Center Sorting Options */}
                 <div className="flex items-center space-x-2">
-                    <button className="text-gray-600 hover:text-gray-800">Popular</button>
+                    {/*<button className="text-gray-600 hover:text-gray-800">Popular</button>
                     <span>|</span>
-                    <button className="text-gray-600 hover:text-gray-800">Recent</button>
-                </div>
+                    <button className="text-gray-600 hover:text-gray-800">Recent</button>*/}                </div>
 
                 {/* Question Count */}
                 <div className="text-gray-600">10 Questions</div>
