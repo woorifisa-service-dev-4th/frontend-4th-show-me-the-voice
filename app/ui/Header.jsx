8f504826dc8fdd25fe00ca57d4b2f261b3bbd78b
luxihua <youngseoma@naver.com>
2025-01-24 15:48:43 +0900

    feat: header 하위 subheader 추가

--- app/ui/Header.jsx
+++ app/ui/Header.jsx
@@ -1,13 +1,10 @@
-import { Bell, Home, Search, User } from "lucide-react"
+import { User } from "lucide-react"
 
 export function Header() {
     return (
-        <header className="flex justify-between items-center p-4 bg-blue-500 text-white">
-            <h1 className="text-xl font-bold">SocialFeed</h1>
+        <header className="flex justify-between items-center p-4 bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 text-white">
+            <h1 className="text-xl font-bold">생생정호통</h1>
             <nav className="flex space-x-4">
-                <Home size={24} />
-                <Search size={24} />
-                <Bell size={24} />
                 <User size={24} />
             </nav>
         </header>
