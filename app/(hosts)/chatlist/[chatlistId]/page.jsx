08c25ba448948601a6a0becd24839ad5106c233e
sengjun0624 <sengjun0624@naver.com>
2025-01-28 08:57:45 +0900

    feat: host페이지 비인가된 접근 홈으로 라우팅 처리

--- app/(hosts)/chatlist/[chatlistId]/page.jsx
+++ app/(hosts)/chatlist/[chatlistId]/page.jsx
@@ -7,7 +7,6 @@ export default function Page() {
     return (
         <>
             <ChatListHeader/>
-            &nbsp;
             <Suspense fallback={<SkeletonLoader/>}>
                 <CardWrapper/>
             </Suspense>
--- app/ui/(host)/chatlist/Header.jsx
+++ app/ui/(host)/chatlist/Header.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 export const ChatListHeader = () => {
     return (
+        <>
             <header className="flex items-center justify-between p-4 bg-white shadow-md sticky top-0 z-10">
                 <Link href="/chatlist">
                     <button className="text-gray-600 hover:text-gray-800">
@@ -10,6 +11,8 @@ export const ChatListHeader = () => {
                     </button>
                 </Link>
             </header>
+            &nbsp;
+        </>
     );
 };
 
