10dfbeac3ecbecb2b9458c9bccf30e5f16fecaa6
sengjun0624 <sengjun0624@naver.com>
2025-01-27 16:29:40 +0900

    feat: 핀 되어있는 카드 배경색 변경

--- app/ui/(host)/chatlist/Cards.jsx
+++ app/ui/(host)/chatlist/Cards.jsx
@@ -1,12 +1,12 @@
 import Card from "@/app/ui/Card";
-import {fetchCards, getCardsByOrderOption} from "@/app/lib/data"
+import {getCardsByOrderOption} from "@/app/lib/data"
 
 export  async function CardWrapper() {
     const cards = await getCardsByOrderOption('popular');
     return (
         <div>
             {cards.map((card) => (
-                <Card key={card.id} id={card.id} content={card.content} username={card.username} likes={card.likes}>
+                <Card key={card.id} card={card}>
                 </Card>
             ))}
         </div>
--- app/ui/Card.jsx
+++ app/ui/Card.jsx
@@ -1,18 +1,23 @@
 import DropdownMenuDemo from "@/app/ui/(host)/chatlist/Dropdown";
 import LikeButton from "@/app/ui/(host)/chatlist/Likes";
 
-export default function Card({id,username,content, likes}) {
-    return (
-            <div className="bg-white p-4 rounded-lg shadow mb-4">
-                <div className={"flex justify-between"}>
-                    <h2 className="font-bold mb-2">{username}</h2>
-                    <DropdownMenuDemo id={id}/>
-                </div>
+export default function Card({card}) {
+    const {id,is_pinned, content, username, likes} = card;
 
-                <p className="mb-4">{content}</p>
-                <div className="flex justify-end text-gray-500">
-                    <LikeButton id={id} initialLikes={likes} />
-                </div>
+    return (
+        <div className={`p-4 rounded-lg shadow mb-4 ${
+            is_pinned ? 'bg-yellow-100' : 'bg-white'
+        }`}>
+            <div className={"flex justify-between"}>
+
+                <h2 className="font-bold mb-2">{username}</h2>
+                <DropdownMenuDemo id={id}/>
             </div>
-        );
+
+            <p className="mb-4">{content}</p>
+            <div className="flex justify-end text-gray-500">
+                <LikeButton id={id} initialLikes={likes}/>
+            </div>
+        </div>
+    );
 }
