ea0df4a910a949ec7ee0ae5ac6527cd76120a303
luxihua <108069902+luxihua@users.noreply.github.com>
2025-02-03 00:11:45 +0000

    feat : 공통 header logo 추가 (#54)

--- app/ui/Header.jsx
+++ app/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { User } from "lucide-react"
+import Image from "next/image"
 
 export function Header() {
     return (
         <header className="flex justify-between items-center p-4 bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 text-white">
-            <h1 className="text-xl font-bold">생생정호통</h1>
+            <div className="flex items-center space-x-2">
+                <Image src="/logo.png" width={100} height={100} alt="logo" />
+            </div>
             <nav className="flex space-x-4">
                 <User size={24} />
             </nav>
